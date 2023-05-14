cdabd7929f3ef743e20c378e09816582762103f7
acnormun <anaclaranoronha.m@gmail.com>
2023-05-14 11:16:02 -0300

    forms

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,32 +1,84 @@
-import {Play} from 'phosphor-react'
-import { CountDownContainer, FormContainer, HomeContainer, MinutesAmoundInput, Separator, StartCountDownButton, TaskInput } from './styles'
+import { Play } from "phosphor-react";
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmoundInput,
+  Separator,
+  StartCountDownButton,
+  TaskInput,
+} from "./styles";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
 
-export function Home(){
-    return (
-        <HomeContainer>
-            <form action="">
-                <FormContainer>
-                    <label htmlFor="task">Vou trabalhar em</label>
-                    <TaskInput type="text" id="task" placeholder='Dê um nome para o seu projeto'/>
-                    
-                    <label htmlFor="minutsAmoud">Por</label>
-                    <MinutesAmoundInput type="number" id="minutesAmound" placeholder='00'/>
-                    <span>minutos</span>
-                </FormContainer>
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmound: zod
+    .number()
+    .min(5, "O ciclo deve ter no mínimo 5 minutos")
+    .max(60, "O ciclo deve ter no máximo 60 minutos"),
+});
 
-                <CountDownContainer>
-                    <span>0</span>
-                    <span>0</span>
-                    <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
-                </CountDownContainer>
+type TTask = zod.infer<typeof newCycleFormValidationSchema>;
 
-                <StartCountDownButton disabled type="submit">
-                    <Play size={24}/>
-                    Começar
-                </StartCountDownButton>
-            </form>
-        </HomeContainer>
-    )
+export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<TTask>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: "",
+      minutesAmound: 0,
+    },
+  });
+  function handleCreateNewCycle(data: TTask) {
+    console.log(data);
+    reset();
+  }
+  const task = watch("task");
+  const isSubmitDisabled = !task;
+
+  return (
+    <HomeContainer>
+      <form action="" onSubmit={handleSubmit(handleCreateNewCycle)}>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            type="text"
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            {...register("task")}
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Estudar React.js" />
+          </datalist>
+
+          <label htmlFor="minutsAmoud">Por</label>
+          <MinutesAmoundInput
+            type="number"
+            id="minutesAmound"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            {...register("minutesAmound", { valueAsNumber: true })}
+          />
+          <span>minutos</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <StartCountDownButton type="submit" disabled={isSubmitDisabled}>
+          <Play size={24} />
+          Começar
+        </StartCountDownButton>
+      </form>
+    </HomeContainer>
+  );
 }
